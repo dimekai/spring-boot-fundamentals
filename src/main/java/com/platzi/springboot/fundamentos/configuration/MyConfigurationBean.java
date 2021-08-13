cae29be3d3a86fdc3a4a188f45fc2876d328f9db
JesusDiaz08 <jesus_kdm@hotmail.com>
2021-08-13 15:07:06 -0500

    Configuracion personalizada de un bean para su dependencia

--- src/main/java/com/platzi/springboot/fundamentos/configuration/MyConfigurationBean.java
+++ src/main/java/com/platzi/springboot/fundamentos/configuration/MyConfigurationBean.java
@@ -1,2 +1,33 @@
-package com.platzi.springboot.fundamentos.configuration;public class MyConfigurationBean {
+package com.platzi.springboot.fundamentos.configuration;
+
+import com.platzi.springboot.fundamentos.bean.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfigurationBean {
+    @Bean
+    public MyBean beanOperation() {
+        return new MyBean2Implement();
+    }
+
+    @Bean
+    public MyOperation beanOperationOperation(){
+        return new MyOperationImplement();
+    }
+
+    @Bean
+    public MyBeanWithDependency beanOperationWithDependency(MyOperation myOperation){
+        return new MyBeanWithDependencyImplement(myOperation);
+    }
+
+    @Bean
+    public MyOwnOperation beanOperationMyOwn() {
+        return new MyOwnOperationImplement();
+    }
+
+    @Bean
+    public MyOwnBeanWithDependency beanOperationMyOwnWithDependency(MyOwnOperation myOwnOperation){
+        return new MyOwnBeanWithDependencyImplement(myOwnOperation);
+    }
 }
