efbacc59455daa5ff072b389e2725191782683bb
JesusDiaz08 <jesus_kdm@hotmail.com>
2021-08-13 15:12:39 -0500

    Main

--- src/main/java/com/platzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/platzi/springboot/fundamentos/FundamentosApplication.java
@@ -1,13 +1,41 @@
 package com.platzi.springboot.fundamentos;
 
+import com.platzi.springboot.fundamentos.bean.MyBean;
+import com.platzi.springboot.fundamentos.bean.MyBeanWithDependency;
+import com.platzi.springboot.fundamentos.bean.MyOwnBeanWithDependency;
+import com.platzi.springboot.fundamentos.component.ComponentDependency;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FundamentosApplication {
+public class FundamentosApplication implements CommandLineRunner{
+
+    private ComponentDependency componentDependency;
+    private MyBean myBean;
+    private MyBeanWithDependency myBeanWithDependency;
+    private MyOwnBeanWithDependency myOwnBeanWithDependency;
+
+    public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency,
+                                  MyBean myBean,
+                                  MyBeanWithDependency myBeanWithDependency,
+                                  MyOwnBeanWithDependency myOwnBeanWithDependency) {
+        this.componentDependency = componentDependency;
+        this.myBean = myBean;
+        this.myBeanWithDependency = myBeanWithDependency;
+        this.myOwnBeanWithDependency = myOwnBeanWithDependency;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FundamentosApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        this.componentDependency.saludar();
+        this.myBean.print();
+        this.myBeanWithDependency.printWithDependency();
+        this.myOwnBeanWithDependency.displayElements();
+    }
 }
