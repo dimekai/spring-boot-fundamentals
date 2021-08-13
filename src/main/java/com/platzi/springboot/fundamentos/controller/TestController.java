8e225828b455597c03b547a9e31e2695acbc93bd
JesusDiaz08 <jesus_kdm@hotmail.com>
2021-08-13 15:44:24 -0500

    TestController para la ruta especificada

--- src/main/java/com/platzi/springboot/fundamentos/controller/TestController.java
+++ src/main/java/com/platzi/springboot/fundamentos/controller/TestController.java
@@ -1,2 +1,17 @@
-package com.platzi.springboot.fundamentos.controller;public class TestController {
+package com.platzi.springboot.fundamentos.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+    
+    @RequestMapping
+    @ResponseBody
+    public ResponseEntity<String> func(){
+        return new ResponseEntity<>("Hello from controller!!", HttpStatus.OK);
+    }
 }
