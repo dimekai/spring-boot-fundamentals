cc5a717d99a3012cc25f2fa29064698545044f79
JesusDiaz08 <jesus_kdm@hotmail.com>
2021-08-13 18:04:12 -0500

    Configuracion para la inyeccion utilizando la notacion @@EnableConfigurationProperties(User.class)

--- src/main/java/com/platzi/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/platzi/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -2,11 +2,14 @@ package com.platzi.springboot.fundamentos.configuration;
 
 import com.platzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.platzi.springboot.fundamentos.bean.MyBeanWithPropertiesImplement;
+import com.platzi.springboot.fundamentos.pojo.User;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(User.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
     private String name;
