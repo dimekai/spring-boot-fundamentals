0bc41295045c8fe906e2f221966d60999ae73631
JesusDiaz08 <jesus_kdm@hotmail.com>
2021-08-13 16:12:49 -0500

    Recuperamos las propiedades de application.properties con @Value y usamos un Bean para concatenarlas

--- src/main/java/com/platzi/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/platzi/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -1,2 +1,24 @@
-package com.platzi.springboot.fundamentos.configuration;public class GeneralConfiguration {
+package com.platzi.springboot.fundamentos.configuration;
+
+import com.platzi.springboot.fundamentos.bean.MyBeanWithProperties;
+import com.platzi.springboot.fundamentos.bean.MyBeanWithPropertiesImplement;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GeneralConfiguration {
+    @Value("${value.name}")
+    private String name;
+
+    @Value("${value.lastname}")
+    private String lastname;
+
+    @Value("${value.random}")
+    private String random;
+
+    @Bean
+    public MyBeanWithProperties func() {
+        return new MyBeanWithPropertiesImplement(this.name, this.lastname);
+    }
 }
